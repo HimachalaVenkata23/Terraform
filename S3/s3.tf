5dbecbe39c4bdce7bd3c735aac2ea0dce76fc15d
Himachala <himachala.venkata06@gmail.com>
2022-05-17 21:21:02 +0530

    Commit S3

---
+++ S3/s3.tf
@@ -0,0 +1,25 @@
+resource "aws_s3_bucket" "b1" {
+
+bucket = "s3-terraform-bucket-lab-new"
+acl    = "public-read"   # or can be "public-read"
+
+tags = {
+Name        = "My bucket"
+Environment = "Dev"
+}
+
+}
+
+
+
+# Upload an object
+
+resource "aws_s3_bucket_object" "object" {
+
+bucket = aws_s3_bucket.b1.id
+key    = "profile"
+acl    = "private"  # or can be "public-read"
+source = "C:/Users/Himachala Venkata/Downloads/myfiles/image.png"
+etag = filemd5("C:/Users/Himachala Venkata/Downloads/myfiles/image.png")
+
+}
